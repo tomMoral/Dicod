22580d3e30707490c550f54c02d42488356e306f
Thomas Moreau <toma.moral@gmail.com>
2016-02-04 10:38:57 -0800

    TST Improve test for DICOD and DICOD2D

--- cdl/tests/test_dicod.py
+++ cdl/tests/test_dicod.py
@@ -1,25 +1,166 @@
 import numpy as np
+import pytest
+
+
 from cdl.multivariate_convolutional_coding_problem import\
     MultivariateConvolutionalCodingProblem
 from cdl.dicod import DICOD, ALGO_GS, ALGO_RANDOM
+from cdl.multivariate_convolutional_coding_problem_2d import \
+    MultivariateConvolutionalCodingProblem2D
+from cdl.dicod2d import DICOD2D
+from scipy.signal import convolve2d
 
 
-def test_dicod_simple():
-    D = np.random.normal(size=(1, 10, 2))
-    z = np.zeros((1, 171))
-    z[0, [30, 50, 65, 85, 100, 130]] = 1
+from faulthandler import dump_traceback_later
+from faulthandler import cancel_dump_traceback_later
+
+
+@pytest.yield_fixture
+def exit_on_deadlock():
+    dump_traceback_later(timeout=3, exit=True)
+    yield
+    cancel_dump_traceback_later()
+
+slow = pytest.mark.skipif(
+    not pytest.config.getoption("--runslow"),
+    reason="need --runslow option to run"
+)
+
+ids = ["GaussS, n_jobs=1, seg=1",
+       "GaussS, n_jobs=2, seg=1",
+       "GaussS, n_jobs=3, seg=1",
+       "GaussS, n_jobs=4, seg=1",
+       "GaussS, n_jobs=2, seg=2",
+       "GaussS, n_jobs=2, seg=4",
+       "GaussS, n_jobs=2, seg=8",
+       "Random, n_jobs=1, seg=1",
+       "Random, n_jobs=2, seg=1",
+       "Random, n_jobs=3, seg=1",
+       # "Random, n_jobs=2, seg=2",
+       # "Random, n_jobs=2, seg=4",
+       # "Random, n_jobs=2, seg=8"
+       ]
+param_array = [
+    (ALGO_GS, 1, 1),
+    (ALGO_GS, 2, 1),
+    (ALGO_GS, 3, 1),
+    (ALGO_GS, 4, 1),
+    (ALGO_GS, 2, 2),
+    (ALGO_GS, 2, 4),
+    (ALGO_GS, 2, 8),
+    (ALGO_RANDOM, 1, 1),
+    (ALGO_RANDOM, 2, 1),
+    (ALGO_RANDOM, 3, 1),
+    # (ALGO_RANDOM, 2, 2),
+    # (ALGO_RANDOM, 2, 4),
+    # (ALGO_RANDOM, 2, 8),
+]
+
+
+@pytest.mark.parametrize("algo,n_jobs,n_seg", param_array, ids=ids)
+def test_dicod_simple(exit_on_deadlock, algo, n_jobs, n_seg):
+    K = 3
+    D = np.random.normal(size=(K, 2, 5))
+    z = np.zeros((K, 100))
+    z[0, [0, 12, 23, 30, 42, 50, 65, 85, 95]] = 1
+    z[1, 67] = 2
     x = np.array([[np.convolve(zk, dk, 'full') for dk in Dk]
                   for Dk, zk in zip(D, z)]).sum(axis=0)
     pb = MultivariateConvolutionalCodingProblem(
-            D, x, lmbd=0.01)
+            D, x, lmbd=0.002)
 
-    for algo, name in [(ALGO_GS, "Gauss-Southwell"),
-                       (ALGO_RANDOM, "Random")]:
-        dicod = DICOD(pb, n_jobs=2, use_seg=2, algorithm=algo,
-                      debug=3)
+    dicod = DICOD(pb, n_jobs=n_jobs, use_seg=n_seg,
+                  algorithm=algo, debug=5, patience=1000)
+    dicod.fit(pb)
+
+    pt = pb.pt*(abs(pb.pt) > pb.lmbd)
+
+    # Assert we recover the right support
+    print(pb.pt.reshape(1, -1).nonzero()[1], '\n',
+          pt.reshape(1, -1).nonzero()[1], '\n',
+          z.reshape(1, -1).nonzero()[1])
+    assert np.all(pt.reshape(1, -1).nonzero()[1] ==
+                  z.reshape(1, -1).nonzero()[1]), (
+        "Cost pt: ", pb.cost(pb.pt), "Cost z: ", pb.cost(z))
+
+
+@pytest.mark.parametrize("algo,n_jobs,n_seg", param_array, ids=ids)
+def test_dicod_simple2d(algo, n_jobs, n_seg):
+    K = 3
+    D = np.random.normal(size=(K, 2, 1, 5))
+    z = np.zeros((K, 1, 100))
+    z[0, 0, [0, 12, 23, 30, 42, 50, 65, 85, 95]] = 1
+    z[1, 0, 67] = 2
+    x = np.array([[convolve2d(zk, dk, 'full') for dk in Dk]
+                  for Dk, zk in zip(D, z)]).sum(axis=0)
+    pb = MultivariateConvolutionalCodingProblem2D(
+            D, x, lmbd=0.002)
+
+    dicod = DICOD2D(pb, n_jobs=n_jobs, w_world=n_jobs, use_seg=n_seg,
+                    algorithm=algo, debug=5, patience=1000)
+    dicod.fit(pb)
+
+    pt = pb.pt*(abs(pb.pt) > pb.lmbd)
+
+    # Assert we recover the right support
+    print(pb.pt.reshape(1, -1).nonzero()[1], '\n',
+          z.reshape(1, -1).nonzero()[1])
+    assert np.all(pt.reshape(1, -1).nonzero()[1] ==
+                  z.reshape(1, -1).nonzero()[1]), (
+        "Cost pt: ", pb.cost(pb.pt), "Cost z: ", pb.cost(z))
+    print("Delta cost: ", pb.cost(pb.pt) - dicod.cost)
+    assert pb.cost(pb.pt) - dicod.cost < 1e-5
+
+
+param_corner = [
+    (0, 0),
+    (0, 1),
+    (1, 0),
+    (1, 1),
+    (-5, 7),
+    (-20, 0),
+]
+
+
+@slow
+@pytest.mark.parametrize("h_pad, w_pad", param_corner)
+def test_dicod_2d_corner(h_pad, w_pad):
+    dim = 2
+    K = 3
+    h_dic = 10
+    w_dic = 10
+    h_cod = 100
+    w_cod = 100
+    n_jobs = 4
+    w_world = 2
+
+    D = np.random.normal(size=(K, dim, h_dic, w_dic))
+    z = np.zeros((K, 1, h_cod, w_cod))
+    x = np.array([[convolve2d(zk[0], dk, 'full') for dk in Dk]
+                  for Dk, zk in zip(D, z)]).sum(axis=0)
+    pb = MultivariateConvolutionalCodingProblem2D(
+            D, x, lmbd=0.002)
+    dicod = DICOD2D(pb, n_jobs=n_jobs, w_world=w_world, use_seg=1,
+                    algorithm=ALGO_GS, debug=5, patience=200,
+                    i_max=1e7, t_max=15)
+    for _ in range(3):
+        D = np.random.normal(size=(K, dim, h_dic, w_dic))
+        z = np.zeros((K, 1, h_cod, w_cod))
+        z[0, 0, h_cod//2+h_pad, w_cod//2+w_pad] = 1
+        print(z.shape)
+        # z[1, 0, 50] = 1
+        # z[2, 50, 99] = 1
+        x = np.array([[convolve2d(zk[0], dk, 'full') for dk in Dk]
+                      for Dk, zk in zip(D, z)]).sum(axis=0)
+        pb = MultivariateConvolutionalCodingProblem2D(
+                D, x, lmbd=0.002)
         dicod.fit(pb)
 
-        # Assert we recover the right support
-        assert np.all(pb.pt.nonzero()[1] == z.nonzero()[1]), (
-            "DICOD algo {} failed to recover the right support"
-            "\n{}".format(name, pb.pt.nonzero()))
+        pt = pb.pt*(abs(pb.pt) > pb.lmbd)
+
+        print(pt.reshape(1, -1).nonzero()[0], '\n',
+              pt.reshape(1, -1).nonzero()[1], '\n',
+              z.reshape(1, -1).nonzero()[1])
+        assert np.all(pt.reshape(1, -1).nonzero()[1] ==
+                      z.reshape(1, -1).nonzero()[1]), (
+            "Cost pt: ", pb.cost(pb.pt), "Cost z: ", pb.cost(z))
