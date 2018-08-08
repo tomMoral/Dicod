e63e167de1e612ae5ea4f033c322afa80522376f
tomMoral <thomas.moreau.2010@gmail.com>
2018-08-08 10:05:25 +0200

    TST add option limiting number of worker in CI

--- conftest.py
+++ conftest.py
@@ -1,4 +1,7 @@
 
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true",
-                     help="run slow tests")
+                     help="run slow tests.")
+    parser.addoption("--max_workers", type=int, default=4,
+                     help="maximal number of worker that can be used in the "
+                     "tests.")
